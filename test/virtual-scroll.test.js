cc11d81a491288f53da1addbfb091c15cd1804ec
Milan Vlaški <milan.vlaski321@gmail.com>
2026-03-31 14:34:33 +0200

    test: extract setup to beforeEach

--- test/virtual-scroll.test.js
+++ test/virtual-scroll.test.js
@@ -1,7 +1,20 @@
-import { describe, expect, test } from "bun:test"
+import { describe, expect, test, beforeEach } from "bun:test"
 import { VirtualScroll } from "../src/virtual-scroll"
 
 describe('Virtual scroll', () => {
+    let vs
+    const items = [{ id: 'item-1' }, { id: 'item-2' }]
+    const ITEM_HEIGHT = 10
+
+    beforeEach(() => {
+        vs = new VirtualScroll({
+            createItem: () => ({ style: {} }),
+            updateItemContent: () => ({}),
+            itemHeight: ITEM_HEIGHT,
+            items,
+            itemsContainer: DUMMY_ITEMS_CONTAINER
+        })
+    })
 
     test('with no items, no items are rendered', () => {
         const vs = new VirtualScroll({
@@ -12,67 +25,32 @@ describe('Virtual scroll', () => {
     })
 
     test('with 0px height, no items are rendered', () => {
-        const vs = new VirtualScroll({
-            createItem: () => ({ style: {} }),
-            updateItemContent: () => ({}),
-            itemHeight: 10,
-            items: [{}],
-            itemsContainer: DUMMY_ITEMS_CONTAINER
-        })
         vs.setHeight(0, 0)
         expect(vs.idDomMap).toBeEmpty()
     })
 
     test('with 1px container height, first item is rendered at height 0', () => {
-        const item = { id: 'unique-id' }
-        const vs = new VirtualScroll({
-            createItem: () => ({ style: {} }),
-            updateItemContent: () => ({}),
-            itemHeight: 10,
-            items: [item],
-            itemsContainer: DUMMY_ITEMS_CONTAINER,
-        })
         vs.setHeight(1, 0)
-        expect(vs.idDomMap.get(item.id)).toBeAtHeight(0)
+        expect(vs.idDomMap.get(items[0].id)).toBeAtHeight(0)
     })
 
     test('two items render when enough height is added', () => {
-        const item1 = { id: 'item-1' }
-        const item2 = { id: 'item-2' }
-        const vs = new VirtualScroll({
-            createItem: () => ({ style: {} }),
-            updateItemContent: () => ({}),
-            itemHeight: 10,
-            items: [item1, item2],
-            itemsContainer: DUMMY_ITEMS_CONTAINER,
-        })
-
         vs.setHeight(1, 0)
-        expect(vs.idDomMap.get(item1.id)).toBeAtHeight(0)
-        expect(vs.idDomMap.get(item2.id)).toBeUndefined() // eh...
+        expect(vs.idDomMap.get(items[0].id)).toBeAtHeight(0)
+        expect(vs.idDomMap.get(items[1].id)).toBeUndefined() // eh...
 
-        vs.setHeight(11, 0)
-        expect(vs.idDomMap.get(item1.id)).toBeAtHeight(0)
-        expect(vs.idDomMap.get(item2.id)).toBeAtHeight(10)
+        vs.setHeight(ITEM_HEIGHT + 1, 0)
+        expect(vs.idDomMap.get(items[0].id)).toBeAtHeight(0)
+        expect(vs.idDomMap.get(items[1].id)).toBeAtHeight(10)
     })
 
     test('offscreen items move into unusedPool', () => {
-        const item1 = { id: 'item-1' }
-        const item2 = { id: 'item-2' }
-        const vs = new VirtualScroll({
-            createItem: () => ({ style: {} }),
-            updateItemContent: () => ({}),
-            itemHeight: 10,
-            items: [item1, item2],
-            itemsContainer: DUMMY_ITEMS_CONTAINER,
-        })
-
-        vs.setHeight(11, 0)
-        expect(vs.idDomMap.get(item1.id)).toBeAtHeight(0)
-        expect(vs.idDomMap.get(item2.id)).toBeAtHeight(10)
+        vs.setHeight(ITEM_HEIGHT + 1, 0)
+        expect(vs.idDomMap.get(items[0].id)).toBeAtHeight(0)
+        expect(vs.idDomMap.get(items[1].id)).toBeAtHeight(10)
 
         vs.setHeight(1, 0)
-        expect(vs.idDomMap.get(item1.id)).toBeAtHeight(0)
+        expect(vs.idDomMap.get(items[0].id)).toBeAtHeight(0)
         expect(vs.unusedPool[0]).toBeAtHeight(10)
     })
 
