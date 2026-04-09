b68f0e43c6ed89dde4dfa03004b9ef7ce7e0d8f2
Milan Vlaški <milan.vlaski321@gmail.com>
2026-04-09 13:13:41 +0200

    test: render

--- test/usecases.test.js
+++ test/usecases.test.js
@@ -16,15 +16,15 @@ describe('Virtual scroll', () => {
     let itemsContainer
 
     const TOTAL_ITEMS = 10
-    const items = Array.from({ length: TOTAL_ITEMS }, (_, i) => ({ id: `item-${i}` }))
+    const items = Array.from({ length: TOTAL_ITEMS }, (_, i) => ({ id: `${i}` }))
     const ITEM_HEIGHT = 10
     const CONTAINER_HEIGHT = 3 * ITEM_HEIGHT
     const ENOUGH_HEIGHT = 10_0000
 
-    
+
     // Write updateItemContent that takes the id as param in the dom
     // Assert that updateItemContent is called with such and such params
-    
+
     // Verify state is preserved when scrolling away
     beforeEach(() => {
         itemsContainer = document.createElement('div')
@@ -37,40 +37,43 @@ describe('Virtual scroll', () => {
         })
     })
 
-    xtest('proper reusability of elements', () => {
+    test('proper reusability of elements', () => {
         vs.setHeight(items.length * ITEM_HEIGHT, 0)
 
-        const evenItems = removeOdd(items)
-        const oddItems = removeEven(items)
+        const evens = removeOdd(items)
+        vs.items = evens
 
-        vs.items = evenItems
+        vs.setHeight(items.length * ITEM_HEIGHT, 0)
 
-        // we expect all the items to be rendered as DOM elements
+        expect(getVisibleElementsTextContent())
+            .toEqual(['id:0', 'id:2', 'id:4', 'id:6', 'id:8',])
+
         // we can even store their references?
         // once we remove the odd ones, we expect the even ones to
         // stay updates 0 times
 
         // and when we add an odd element
         // and we expect the even n
-        vs.setHeight(items.length * ITEM_HEIGHT, 0)
+        // vs.setHeight(items.length * ITEM_HEIGHT, 0)
 
-        expect(getChildrenHtml(vs)).toEqual(['id:1', 'id:2', 'id:3'])
-        expect(vs.idDomMap.get(items[0].id)).toBeUpdated(3)
+        // expect(vs.idDomMap.get(items[0].id)).toBeUpdated(3)
     })
 
-    const removeOdd = arr => arr.splice((_, i) => i % 2 === 0)
-    const removeEven = arr => arr.filter((_, i) => i % 2 !== 0)
+    const removeOdd = arr => arr.filter((_, i) => i % 2 === 0)
 
     // TODO helper function that takes innerHTML of each element,
     // so we can inspect it
     // Give sorted elements
-
+    function getVisibleElementsTextContent() {
+        return Array.from(vs.idDomMap.values())
+            .sort((a, b) => a.id - b.id)
+            .map(child => child.textContent)
+    }
 
     // helpers
     const scrollToPosition = (position) => {
         vs.setHeight(CONTAINER_HEIGHT, position * ITEM_HEIGHT)
-        console.log(`Container at ${position}`,JSON.stringify(itemsContainer.innerHTML, null, 2))
-
+        console.log(`Container at ${position}`, JSON.stringify(itemsContainer.innerHTML, null, 2))
     }
 
 
